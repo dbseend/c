5ab7b5a19b0e5094bea8b726e9b6d7a6b3be29ba
윤성현 <shyoon840@gmail.com>
2024-11-20 11:00:12 +0000

    craete nextjs project

---
+++ supabase-note/component/header.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-green-700">
+      <img src="/public/supanote.png" alt="supanote logo" className="h-7" />
+    </header>
+  );
+}
